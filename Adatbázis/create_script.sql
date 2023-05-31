4c0e4c2f299d4841b5801ad99d2989a6fdaa7413
BerenyiPeti <berenyipeti01@gmail.com>
2023-05-31 14:16:55 +0200

    create script

--- Adatbázis/create_script.sql
+++ Adatbázis/create_script.sql
@@ -5,4 +5,26 @@ go
 
 use vizsga
 go
+
+CREATE TABLE [dbo].[kategoria](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[kategorianev] [nvarchar](max) NOT NULL,
+	primary key (id)
+)
+go
+
+CREATE TABLE [dbo].[teszt](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[kerdes] [nvarchar](max) NOT NULL,
+	[v1] [nvarchar](max) NOT NULL,
+	[v2] [nvarchar](max) NOT NULL,
+	[v3] [nvarchar](max) NOT NULL,
+	[v4] [nvarchar](max) NOT NULL,
+	[helyes] [nvarchar](max) NOT NULL,
+	[kategoriaId] [int] NOT NULL,
+primary key (id)
+)
+go
+
 alter table teszt add foreign key (kategoriaId) references kategoria(id)
+
